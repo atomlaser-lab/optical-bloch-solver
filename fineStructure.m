d2b99f57ac784b06e392588ee501d34d44dbecf3
Ryan Thomas <ryan.j.thomas1@gmail.com>
2021-05-20 17:12:07 +1000

    Added comments to laser and fineStructure classes
    Also changed how laser class handles intensity, with separate
    function

--- fineStructure.m
+++ fineStructure.m
@@ -1,205 +1,311 @@
 classdef fineStructure < handle
+    %FINESTRUCTURE Defines a class that represents the fine structure and
+    %hyperfine structure of a state in an alkali metal atom
     properties
-        species
-        numStates
-        S
-        L
-        J
-        I
-        gI
-        gJ
-        A1
-        A2
+        species         %The species of atom
+        numStates       %The total number of states
         
-        E
-        H0
-        U1int
-        U31
-        U3int
-        BV1
-        BV3
+        L               %The orbital angular momentum
+        J               %The orbital + electron spin angular momentum
+        I               %The nuclear angular momentum
+        
+        gI              %The nuclear g-factor
+        gJ              %The orbital+electron g-factor
+        A1              %The magnetic dipole constant in MHz
+        A2              %The electric quadrupole constant in MHz
+        
+        E               %The state energies
+        H0              %The bare Hamiltonian in the uncoupled basis |mJ,mI>
+        U1int           %The transformation unitary from the "internal" basis with magnetic field to the uncoupled basis
+        U31             %The transformation unitary from the uncoupled basis to the |F,mF> basis
+        U3int           %The transformation unitary from the "internal" basis to the |F,mF> basis
+        BV1             %The basis vectors of the uncoupled basis listed as [mJ,mI]
+        BV3             %The basis vectors of the |F,mF> basis listes as [F,mF]
+    end
+    
+    properties(Constant)
+        S = 0.5;        %The spin of the electron
     end
 
     methods
-        function obj=fineStructure(species,L,J)
-            obj.S=0.5;
-            obj.L=L;
-            obj.J=J;
-            if strcmpi(species,'Rb87'),
-                obj.species='Rb87';
-                obj.I=3/2;
-                obj.gI=-0.0009951414;
-            elseif strcmpi(species,'K40'),
-                obj.species='K40';
-                obj.I=4;
-                obj.gI=0.000176490;
-            elseif strcmpi(species,'K41'),
-                obj.species='K41';
-                obj.I=3/2;
-                obj.gI=0.000176490;
-            end;
+        function self = fineStructure(species,L,J)
+            %FINESTRUCTURE Creates an object of class FINESTRUCTURE
+            %
+            %   FS = FINESTRUCTURE(SPECIES,L,J) creates a FINESTRUCTURE
+            %   object FS corresponding to SPECIES and with orbital angular
+            %   momentum L and orbital+electron spin angular momentum J.
+            %   SPECIES can be one of 'Rb87', 'K40', or 'K41'.
+            self.L = L;
+            self.J = J;
+            if strcmpi(species,'Rb87')
+                self.species = 'Rb87';
+                self.I = 3/2;
+                self.gI = -0.0009951414;
+            elseif strcmpi(species,'K40')
+                self.species = 'K40';
+                self.I = 4;
+                self.gI = 0.000176490;
+            elseif strcmpi(species,'K41')
+                self.species = 'K41';
+                self.I = 3/2;
+                self.gI = 0.000176490;
+            end
             
-            if obj.L==0,
-                obj.setGroundState;
-            elseif obj.L==1 && obj.J==0.5,
-                obj.setP1_2;
-            elseif obj.L==1 && obj.J==1.5,
-                obj.setP3_2;
-            end;
-            obj.gJ=obj.calcLandeJ(obj.S,obj.L,obj.J);
-            obj.numStates=(2*obj.I+1)*(2*obj.J+1);
-            obj.solveHyperfine(0);
-        end;    %end fineStructure
-        
-        function obj=makeH0(obj)
-            numI=2*obj.I+1;
-            numJ=2*obj.J+1;
-            nDim=obj.numStates;
-            mJ=-obj.J:obj.J;
-            mI=-obj.I:obj.I;
-            %Uncoupled basis |mJ,mI>
-            obj.BV1=[reshape(repmat(mJ,numI,1),nDim,1) repmat(mI(:),numJ,1)];
-            
-            %I-J coupled basis |F,mF>. Ordered by increasing energy for low magnetic fields
-            obj.BV3=zeros(nDim,2);
-            if obj.A1>0,
-                F=abs(obj.I-obj.J):abs(obj.I+obj.J);
+            if self.L == 0
+                %
+                % L = 0 is a ground state
+                %
+                self.setGroundState;
             else
-                F=abs(obj.I+obj.J):-1:abs(obj.I-obj.J);
-            end;
-            mm=1;
-            for nn=1:numel(F),
-                gF=obj.calcLandeF(obj.I,obj.J,F(nn),obj.gI,obj.gJ);
-                if gF>0,
-                    mF=-F(nn):F(nn);
-                else
-                    mF=F(nn):-1:-F(nn);
-                end;
-                obj.BV3(mm:(mm+2*F(nn)),:)=[F(nn)*ones(2*F(nn)+1,1) mF(:)];
-                mm=mm+2*F(nn)+1;
-            end;
+                %
+                % L ~= 0 is an excited stae
+                %
+                self.setExcitedState;
+            end
+            %
+            % Calculate the Lande g-factor and the number of states
+            %
+            self.gJ = self.calcLandeJ(self.S,self.L,self.J);
+            self.numStates = (2*self.I+1)*(2*self.J+1);
+            %
+            % Solve for the hyperfine states in the absence of a magnetic
+            % field just so that the necessary vectors are populated
+            % correctly
+            %
+            self.solveHyperfine(0);
+        end
+        
+        function self = makeH0(self)
+            %MAKEH0 Creates the "bare" Hamiltonian in the absence of a
+            %magnetic field
+            %
+            %   FS = FS.MAKEH0 creates the "bare" Hamiltonian
             
-            %Transformation matrix from uncoupled |mJ,mI> basis to the coupled |F,mF> basis
-            %Inverse is the hermitian conjugate
-            obj.U31=zeros(nDim);
-            for a=1:nDim,
-                F=obj.BV3(a,1);
-                mF=obj.BV3(a,2);
-                for b=1:nDim,
-                    mJ=obj.BV1(b,1);
-                    mI=obj.BV1(b,2);
-                    if abs(mI+mJ)>F || (mI+mJ)~=mF,
+            %
+            % Create the basis vectors in the "uncoupled basis of |mJ,mI>
+            %
+            numI = 2*self.I+1;
+            numJ = 2*self.J+1;
+            nDim = self.numStates;
+            mJ = -self.J:self.J;
+            mI = -self.I:self.I;
+            self.BV1=[reshape(repmat(mJ,numI,1),nDim,1) repmat(mI(:),numJ,1)];  %Uncoupled basis |mJ,mI>
+            %
+            % I-J coupled basis |F,mF>. Ordered by increasing energy for low magnetic fields
+            %
+            self.BV3 = zeros(nDim,2);
+            if self.A1 > 0
+                %
+                % If A1 > 0 then larger F corresponds to higher energies
+                %
+                F = abs(self.I-self.J):abs(self.I+self.J);
+            else
+                %
+                % If A1 < 0 then larger F corresponds to smaller energies
+                %
+                F = abs(self.I+self.J):-1:abs(self.I-self.J);
+            end
+            %
+            % Populate basis vector with mF labels based on Lande g-factor
+            % for each hyperfine manifold
+            %
+            mm=1;
+            for nn=1:numel(F)
+                gF = self.calcLandeF(self.I,self.J,F(nn),self.gI,self.gJ);
+                if gF > 0
+                    %
+                    % If gF > 0 then higher mF values are higher in energy
+                    %
+                    mF = -F(nn):F(nn);
+                else
+                    %
+                    % If gF < 0 then higher mF values are lower in energy
+                    %
+                    mF = F(nn):-1:-F(nn);
+                end
+                %
+                % This puts the state labels in the correct location in the
+                % BV3 array
+                %
+                self.BV3(mm:(mm+2*F(nn)),:) = [F(nn)*ones(2*F(nn)+1,1) mF(:)];
+                mm = mm+2*F(nn)+1;
+            end
+            %
+            % Transformation matrix from uncoupled |mJ,mI> basis to the coupled |F,mF> basis
+            % Inverse is the hermitian conjugate
+            %
+            self.U31 = zeros(nDim);
+            for a = 1:nDim
+                F = self.BV3(a,1);
+                mF = self.BV3(a,2);
+                for b = 1:nDim
+                    mJ = self.BV1(b,1);
+                    mI = self.BV1(b,2);
+                    if (abs(mI + mJ) > F) || ((mI + mJ) ~= mF)
+                        %
+                        % Sum of m values must the equal. Also mI + mJ must
+                        % always be less than F
+                        %
                         continue;
                     else
-                        obj.U31(a,b)=ClebschGordan(obj.I,obj.J,F,mI,mJ,mF);
-                    end;
-                end;
-            end;
-            
-            %% Bare Hamiltonian calculated in uncoupled basis
-            H=zeros(nDim);
-            F=abs(obj.I-obj.J):abs(obj.I+obj.J);
-            for a=1:nDim,
-                mJ1=obj.BV1(a,1);
-                mI1=obj.BV1(a,2);
-                for b=1:nDim,
-                    mJ2=obj.BV1(b,1);
-                    mI2=obj.BV1(b,2);
-                    for c=1:numel(F),
-                        if abs(mI1+mJ1)>F(c) || abs(mI2+mJ2)>F(c) || (mI1+mJ1)~=(mI2+mJ2),
+                        self.U31(a,b) = ClebschGordan(self.I,self.J,F,mI,mJ,mF);
+                    end
+                end
+            end
+            %
+            % Finally calculate the bare Hamiltonian in the uncoupled basis
+            % Based on Steck's Rubidium data https://steck.us/alkalidata/rubidium87numbers.1.6.pdf
+            % equation 15
+            %
+            H = zeros(nDim);
+            F = abs(self.I-self.J):abs(self.I+self.J);
+            for a=1:nDim
+                mJ1 = self.BV1(a,1);
+                mI1 = self.BV1(a,2);
+                for b = 1:nDim
+                    mJ2 = self.BV1(b,1);
+                    mI2 = self.BV1(b,2);
+                    for c = 1:numel(F)
+                        if (abs(mI1 + mJ1) > F(c)) || (abs(mI2 + mJ2) > F(c)) || ((mI1 + mJ1) ~= (mI2 + mJ2))
                             continue;
-                        end;
-                            CG=ClebschGordan(obj.I,obj.J,F(c),mI1,mJ1,mI1+mJ1).*ClebschGordan(obj.I,obj.J,F(c),mI2,mJ2,mI2+mJ2);
-                            K=F(c)*(F(c)+1)-obj.I*(obj.I+1)-obj.J*(obj.J+1);
-                            if obj.J>=1.0 && obj.I>=1 && obj.L>0,
-                                tmp=CG*(obj.A1/2*K+obj.A2*(1.5*K*(K+1)-2*obj.I*obj.J*(obj.I+1)*(obj.J+1))./(4*obj.I*obj.J*(2*obj.I-1)*(2*obj.J-1)));
+                        end
+                            CG = ClebschGordan(self.I,self.J,F(c),mI1,mJ1,mI1+mJ1).*ClebschGordan(self.I,self.J,F(c),mI2,mJ2,mI2+mJ2);
+                            K = F(c)*(F(c)+1)-self.I*(self.I+1)-self.J*(self.J+1);
+                            if self.J >= 1 && self.I >= 1 && self.L > 0
+                                %
+                                % This only applies for L > 0, I > 1, and J
+                                % > 1. Needs a conditional otherwise we get
+                                % a NaN value
+                                %
+                                tmp = CG*(self.A1/2*K+self.A2*(1.5*K*(K+1)-2*self.I*self.J*(self.I+1)*(self.J+1))./(4*self.I*self.J*(2*self.I-1)*(2*self.J-1)));
                             else
-                                tmp=CG*obj.A1/2*K;
-                            end;
-                            H(a,b)=H(a,b)+tmp;
-                    end;
-                end;
-            end;
-            obj.H0=H;
-        end;    %%end makeH0
+                                tmp = CG*self.A1/2*K;
+                            end
+                            H(a,b) = H(a,b)+tmp;
+                    end
+                end
+            end
+            %
+            % Assign calculated Hamiltonian to H0
+            %
+            self.H0 = H;
+        end
         
-        function [E,U1int]=solveHyperfine(obj,B)
-            mu_B=9.27400968e-24/(6.62606957e-34*1e4)/1e6;
-            if numel(obj.H0)==0,
-                obj.makeH0;
-            end;
-            %% Zeeman field
-            %E is the eigen-energies as a matrix
-            %U1int transforms a vector from the diagonal basis to the uncoupled basis
-            if B==0,
-                %No field case is diagonal in |F,mF> basis
-                E=obj.U31*obj.H0*(obj.U31');
-                U1int=obj.U31';
+        function [E,U1int] = solveHyperfine(self,B)
+            %SOLVEHYPERFINE Solves the hyperfine + Zeeman Hamiltonian for
+            %the energies and eigenvectors
+            %
+            %   E = FS.SOLVEHYPERFINE(B) Solves the Hamiltonian for a given
+            %   magnetic field B (in Gauss) for energies in E in MHz. Is a
+            %   vector of energies ordered from lowest to highest
+            %
+            %   {E,U1int] = FS.SOLVEHYPERFINE(B) Generates a diagonal
+            %   matrix of energies E in MHz and a transformation unitary
+            %   U1int which takes vectors in the basis that solves the
+            %   hyperfine + Zeeman Hamiltonian and transforms them to the
+            %   uncoupled basis |mJ,mI>
+            %
+
+            %
+            % If H0 doesn't exist, make it
+            %
+            if numel(self.H0) == 0
+                self.makeH0;
+            end
+            %
+            % Calculate Zeeman field
+            %
+            if B == 0
+                %
+                % No field case is diagonal in |F,mF> basis
+                %
+                E = self.U31*self.H0*(self.U31');
+                U1int = self.U31';
             else
-                %With a field it is not diagonal in either basis
-                HB=diag(mu_B*B*(obj.gJ*obj.BV1(:,1)+obj.gI*obj.BV1(:,2)));
-                H2=obj.H0+HB;
-                [U1int,E]=eig(H2);  
-            end;
+                %
+                % With a field it is not diagonal in either basis
+                %
+                HB = diag(const.muBh*B*(self.gJ*self.BV1(:,1)+self.gI*self.BV1(:,2)));
+                H2 = self.H0+HB;
+                [U1int,E] = eig(H2);  
+            end
             
-            obj.E=E;
-            obj.U1int=U1int;
-            obj.U3int=obj.U31*obj.U1int;
-            if nargout<=1,
-                E=diag(E);
-            end;
-        end;    %end solveHyperfine
+            self.E=E;
+            self.U1int=U1int;
+            self.U3int=self.U31*self.U1int;
+            if nargout <= 1
+                E = diag(E);
+            end
+        end
         
         
     end
 
     methods(Access = protected)
-        function obj=setGroundState(obj)
-            obj.A2=0;
-            if strcmpi(obj.species,'Rb87'),
-                obj.A1=6834.682610904/(obj.I+obj.S);
-            elseif strcmpi(obj.species,'K40'),
-                obj.A1=-285.7308;
-            elseif strcmpi(obj.species,'K41'),
-                obj.A1=254.013872/(obj.I+obj.S);
-            end;
-        end;
+        function self = setGroundState(self)
+            %SETGROUNDSTATE Sets properties correctly for a ground state
+            % (L = 0).
+            %
+            %   FS = FS.SETGROUNDSTATE() Sets the ground state properties
+            self.A2 = 0;
+            if strcmpi(self.species,'Rb87')
+                self.A1 = 6834.682610904/(self.I+self.S);
+            elseif strcmpi(self.species,'K40')
+                self.A1 = -285.7308;
+            elseif strcmpi(self.species,'K41')
+                self.A1 = 254.013872/(self.I+self.S);
+            end
+        end
         
-        function obj=setP1_2(obj)
-            obj.A2=0;
-            if strcmpi(obj.species,'Rb87'),
-                obj.A1=408.328;
-            elseif strcmpi(obj.species,'K40'),
-                obj.A1=-34.523;
-            elseif strcmpi(obj.species,'K41'),
-                obj.A1=15.245;
-            end;
-        end;    %end setP1_2
-        
-        function obj=setP3_2(obj)
-            
-            if strcmpi(obj.species,'Rb87'),
-                obj.A1=84.7185;
-                obj.A2=12.4965;
-            elseif strcmpi(obj.species,'K40'),
-                obj.A1=-7.585;
-                obj.A2=-3.445;
-            elseif strcmpi(obj.species,'K41'),
-                obj.A1=3.363;
-                obj.A2=3.351;
-            end;
-        end;    %end setP3_2
+        function self = setExcitedState(self)
+            %SETEXCITEDSTATE Sets properties correctly for an excited state
+            % (L = 0).
+            %
+            %   FS = FS.SETEXCITEDSTATE() Sets the excited state properties
+            if self.J == 0.5
+                self.A2 = 0;
+                if strcmpi(self.species,'Rb87')
+                    self.A1 = 6834.682610904/(self.I+self.S);
+                elseif strcmpi(self.species,'K40')
+                    self.A1 = -285.7308;
+                elseif strcmpi(self.species,'K41')
+                    self.A1 = 254.013872/(self.I+self.S);
+                end
+            elseif self.J == 1.5
+                if strcmpi(self.species,'Rb87')
+                    self.A1 = 84.7185;
+                    self.A2 = 12.4965;
+                elseif strcmpi(self.species,'K40')
+                    self.A1 = -7.585;
+                    self.A2 = -3.445;
+                elseif strcmpi(self.species,'K41')
+                    self.A1 = 3.363;
+                    self.A2 = 3.351;
+                end
+            end
+        end
     end
 
     
     methods(Static)
-        function gJ=calcLandeJ(S,L,J)
-            gJ=(J.*(J+1)-S*(S+1)+L*(L+1))./(2*J.*(J+1))+2*(J.*(J+1)+S*(S+1)-L*(L+1))/(2*J*(J+1));
-        end;    %end calcLandeJ
+        function gJ = calcLandeJ(S,L,J)
+            %CALCLANDEJ Calculates the Lande g-factor for a J state
+            %
+            %   gJ = CALCLANDE(S,L,J) calculates the Lande g-factor for the
+            %   state labelled with electron spin S, orbital angular
+            %   momentum L and total angular momentum J
+            gJ = (J.*(J+1)-S*(S+1)+L*(L+1))./(2*J.*(J+1))+2*(J.*(J+1)+S*(S+1)-L*(L+1))/(2*J*(J+1));
+        end
         
-        function gF=calcLandeF(I,J,F,gI,gJ)
-            gF=gJ*(F.*(F+1)-I*(I+1)+J*(J+1))./(2*F.*(F+1))+gI*(F.*(F+1)+I.*(I+1)-J.*(J+1))./(2*F.*(F+1));
-        end;    %end calcLandeF
+        function gF = calcLandeF(I,J,F,gI,gJ)
+            %CALCLANDEF Calculates the Lande g-factor for an F state
+            %
+            %   gF = CALCLANDE(I,J,F,gI,gJ) calculates the Lande g-factor for the
+            %   state labelled with nuclear spin I, angular+electron spin
+            %   J, and total angular momentum F with nuclear g-factor gI
+            %   and J g-factor gJ
+            gF = gJ*(F.*(F+1)-I*(I+1)+J*(J+1))./(2*F.*(F+1))+gI*(F.*(F+1)+I.*(I+1)-J.*(J+1))./(2*F.*(F+1));
+        end
     end
     
     
--- laser.m
+++ laser.m
@@ -1,67 +1,116 @@
 classdef laser < handle
+    %LASER Defines a class that represents a laser beam
     properties
-        P
-        w0
+        P           %Optical power in W
+        w0          %Beam waist in m^2
         
-        intensity
-        field
-        pol
+        intensity   %Intensity in W/m^2
+        field       %Electric field in V/m
+        pol         %Polarization as 3-element vector in either spherical [sigma_-,pi,sigma_+] or linear [x,y,z] polarization
         
-        %|F,mF> specifying state that the laser is detuned from
-        ground  
-        excited
-        detuning
+        ground      %|F,mF> ground state to use as a reference for the detuning
+        excited     %|F',mF'> excited state to use a s a reference for the detuning
+        detuning    %The detuning of the laser from the reference transition
     end
     
     properties(Constant)
-        sphPolBasis=[1 1i 0;0 0 1;-1 1i 0]; %Converts (x,y,z) components to q=(-1,0,1) (spherical) components
+        sphPolBasis = [1 1i 0;0 0 1;-1 1i 0]; %Converts (x,y,z) components to q=(-1,0,1) (spherical) components
     end
     
     methods
-        function obj=laser(P,w0)
-            obj.detuning=0;
-            if nargin~=0,
-                obj.setGaussBeam(P,w0);
-            end;
-        end;
+        function self = laser(P,w0)
+            %LASER Creates a LASER object
+            %
+            %   L = LASER(I) Creates a LASER object with intensity I
+            %
+            %   L = LASER(P,W) Creates a LASER object corresponding to a
+            %   Gaussian beam with total power P and beam waist W
+            self.detuning = 0;
+            if nargin == 1
+                self.setIntensity(I);
+            elseif nargin == 2
+                self.setGaussBeam(P,w0);
+            end
+        end
         
-        function obj=setGaussBeam(obj,P,w0)
-            if nargin==2,
-                obj.P=P;
-            elseif nargin==3,
-                obj.P=P;
-                obj.w0=w0;
-            end;
-            obj.field=sqrt(obj.P./(pi*const.eps0*const.c*obj.w0^2));
-            obj.intensity=2*const.eps0*const.c*obj.field.^2;
-        end;    %end setGaussBeam
+        function self = setIntensity(self,I)
+            %SETINTENSITY Sets the intensity of the laser field
+            %
+            %   L = L.SETINTENSITY(I) Sets the intensity of the laser field
+            %   to I. Automatically calculates the new electric field
+            self.intensity = I;
+            self.field = self.calcField(self.intensity);
+        end
         
-        function obj=setPolarization(obj,pol,polBasis)
-            if nargin==2,
-                obj.pol=pol(:);
-            elseif strcmpi(polBasis,'linear'),
-                obj.pol=obj.sphPolBasis*pol(:);
-            elseif strcmpi(polBasis,'spherical'),
-                obj.pol=pol(:);
+        function self = setGaussBeam(self,P,w0)
+            %SETGAUSSBEAM Sets intensity based on the assumption of a
+            %Gaussian beam
+            %
+            %   L = L.SETGAUSSBEAM(P,W) sets the intensity and electric
+            %   field assuming a Gaussian beam of total power P and beam
+            %   waist W
+            self.intensity = 2*P/(pi*w0^2);
+            self.field = self.calcFieldF(self.intensity);
+        end
+        
+        function self = setPolarization(self,pol,polBasis)
+            %SETPOLARIZATION Sets the polarization of the field. Function
+            %automatically normalises the input polarization to have unit
+            %length
+            %
+            %   L = L.SETPOLARIZATION(POL) Sets the polarization to the
+            %   polarization POL assuming that POL is in the spherical
+            %   basis q = [-1,0,1].
+            %
+            %   L = L.SETPOLARIZATION(__,BASIS) uses BASIS (either 'linear'
+            %   or 'spherical') to set the polarization.  If 'linear',
+            %   assumes polarization is in [x,y,z] order
+            if nargin == 2
+                self.pol = pol(:);
+            elseif strcmpi(polBasis,'linear')
+                self.pol = self.sphPolBasis*pol(:);
+            elseif strcmpi(polBasis,'spherical')
+                self.pol = pol(:);
             else
                 error('Polarization basis not supported!');
-            end;
-            obj.pol=obj.pol./sqrt(obj.pol'*obj.pol);
-        end;    %end setPolarization
+            end
+            self.pol = self.pol./sqrt(self.pol'*self.pol);
+        end
         
-        function obj=setStates(obj,ground,excited,detuning)
-           if nargin==2,
-               obj.ground=ground;
-           elseif nargin==3,
-               obj.ground=ground;
-               obj.excited=excited;
+        function self = setStates(self,ground,excited,detuning)
+            %SETSTATES Sets the reference transition/states and the
+            %detuning from that transition
+            %
+            %   L = L.SETSTATES(GROUND) Sets the reference ground state as
+            %   a 2-element vector [F,mF].
+            %
+            %   L = L.SETSTATES(__,EXCITED) Sets the reference excited
+            %   state as a 2-element vector [F,mF]
+            %
+            %   L = L.SETSTATUS(__,DETUNING) Sets the detuning from the
+            %   reference states/transition to be DETUNING
+           if nargin == 2
+               self.ground = ground;
+           elseif nargin == 3
+               self.ground = ground;
+               self.excited = excited;
            elseif nargin == 4
-               obj.ground = ground;
-               obj.excited = excited;
-               obj.detuning = detuning;
-           end;
-        end;    %end setDetuning
+               self.ground = ground;
+               self.excited = excited;
+               self.detuning = detuning;
+           end
+        end
         
+    end
+    
+    methods(Static)
+        function E = calcField(I)
+            %CALCFIELD Calculates the electric field for a given intensity
+            %
+            %   E = CALCFIELD(I) Calculates the electric field given
+            %   intensity I
+            E = sqrt(0.5*I./(const.eps0*const.c));
+        end
     end
     
 end
